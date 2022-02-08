433f9ceee640984078b642e6c1985a4b5d4bdf8f
kyrylo <kyrylo.onufriiev@applitools.com>
2022-02-08 22:17:02 +0200

    fix

--- packages/screenshoter/test/e2e/ios-web/page-fully-landscape.spec.js
+++ packages/screenshoter/test/e2e/ios-web/page-fully-landscape.spec.js
@@ -1,4 +1,4 @@
-const {makeDriver, test} = require('../e2e')
+const {makeDriver, sleep, test} = require('../e2e')
 
 describe('screenshoter ios web', () => {
   const logger = {log: () => {}, warn: () => {}, error: () => {}, verbose: () => {}}
@@ -6,7 +6,6 @@ describe('screenshoter ios web', () => {
 
   before(async () => {
     ;[driver, destroyDriver] = await makeDriver({type: 'ios', app: 'safari', orientation: 'landscape', logger})
-    await driver.visit('https://applitools.github.io/demo/TestPages/PageWithBurgerMenu/')
   })
 
   after(async () => {
@@ -14,6 +13,9 @@ describe('screenshoter ios web', () => {
   })
 
   it('take full page screenshot with landscape orientation', async () => {
+    await driver.visit('https://applitools.github.io/demo/TestPages/PageWithBurgerMenu/')
+    await sleep(3000)
+
     await test({
       type: 'ios-web',
       tag: 'page-fully-landscape',
