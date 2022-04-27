3a320bb99e7a859fe2fe79983147d85f231e0dea
Dave Haeffner <dhaeffner@gmail.com>
2022-04-27 16:17:43 -0400

    uni: add simple queueing mechanism and drop unneeded dependency

--- packages/eyes-universal/src/selenium-proxy.js
+++ packages/eyes-universal/src/selenium-proxy.js
@@ -2,27 +2,45 @@ const express = require('express')
 const morgan = require("morgan")
 const {createProxyMiddleware} = require('http-proxy-middleware')
 
+class Queue {
+  constructor() {
+    this.q = []
+  }
+  size() {
+    return this.q.length
+  }
+  add(item)  {
+    this.q.push(item)
+  }
+  remove() {
+    return this.q.shift()
+  }
+}
+
+const queue = new Queue()
 const app = express()
 const PORT = 4444
 const HOST = "localhost"
 const HUB_URL = "http://localhost:4445/wd/hub"
 app.use(morgan('dev'))
 
-async function q() {
-  return new Promise(res => setTimeout(res, 5000))
+async function q(limit = 1) {
+  if (queue.size() === limit) {
+    console.log('waiting for available session, polling...')
+    await new Promise(res => setTimeout(res, 5000))
+    return q()
+  }
 }
 
 app.use(async (req, res, next) => {
   if (req.path === '/wd/hub/session' && req.method === 'POST') {
-    console.log('concurrency limit reached, waiting for an available session')
-    const start = Date.now()
     await q()
-    const end = Date.now()
-    console.log(`done in ${end - start}ms`)
-    next()
-  } else {
-    next()
+    queue.add('session')
+  } else if (req.path.includes('/wd/hub/session') && req.method === 'DELETE') {
+    queue.remove()
   }
+  console.log('q size', queue.size())
+  next()
 })
 
 app.use('/wd/hub', createProxyMiddleware({
