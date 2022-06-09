7b6fc3ac03fe2b6a77ed62e6aaea1aaba065e3cd
kyrylo <kyrylo.onufriiev@applitools.com>
2022-06-09 22:14:54 +0300

    fix

--- .github/actions/run/index.js
+++ .github/actions/run/index.js
@@ -1,5 +1,5 @@
 import * as core from '@actions/core'
-import {spawnSync} from 'child_process'
+import {execSync} from 'child_process'
 
 const workflow = core.getInput('workflow', {required: true})
 const ref = core.getInput('ref')
@@ -7,14 +7,14 @@ const ref = core.getInput('ref')
 console.log(workflow, ref)
 
 async function main() {
-  spawnSync('gh', ['workflow', 'run', workflow])
-  const {stdout} = spawnSync('gh', ['run', 'list', '--json', 'databaseId', '--workflow', workflow, '--limit', '1'])
+  execSync('gh', ['workflow', 'run', workflow])
+  const {stdout} = execSync('gh', ['run', 'list', '--json', 'databaseId', '--workflow', workflow, '--limit', '1'])
 
   console.log(stdout)
 
   const [{databaseId}] = JSON.parse(stdout)
 
-  console.log(databaseId)
+  execSync('gh', ['run', 'watch', databaseId], {stdio: 'inherit'})
 
 }
 
