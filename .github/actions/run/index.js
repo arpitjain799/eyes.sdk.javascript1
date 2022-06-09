7ae77288ad455b4ce71553e1b5fb3f1c375e78d8
kyrylo <kyrylo.onufriiev@applitools.com>
2022-06-09 20:13:50 +0300

    fix

--- .github/actions/run/index.js
+++ .github/actions/run/index.js
@@ -4,7 +4,7 @@ import {spawnSync} from 'child_process'
 const workflow = core.getInput('workflow', {required: true})
 const ref = core.getInput('ref')
 
-console.log(workflow, ref)
+console.log(workflow, ref, `@${process.env.GITHUB_TOKEN}+`)
 
 const run = spawnSync('gh', ['workflow', 'run', workflow], {
   encoding: 'utf8'
