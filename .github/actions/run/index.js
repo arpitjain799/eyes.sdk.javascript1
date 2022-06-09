7548b77a2da422ee11894b4155ff56f207e106b5
kyrylo <kyrylo.onufriiev@applitools.com>
2022-06-09 19:54:44 +0300

    fix

--- .github/actions/run/index.js
+++ .github/actions/run/index.js
@@ -1,16 +1,16 @@
 import * as core from '@actions/core'
-import {spawn} from 'child_process'
+import {spawnSync} from 'child_process'
 
 const workflow = core.getInput('workflow', {required: true})
 const ref = core.getInput('ref')
 
 console.log(workflow, ref)
 
-const run = spawn(`gh workflow run ${workflow}`, {
+const run = spawnSync(`gh workflow run ${workflow}`, {
   shell: true,
-  stdio: 'inherit'
+  encoding: 'utf8'
 }, console.log)
 
-run.on('error', console.log)
+console.log(run)
 
 setTimeout(() => console.log('HELLO!'), 10_000)
