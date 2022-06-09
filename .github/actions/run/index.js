31d15c3ef75717b19fd2b34b05216c73e5791807
kyrylo <kyrylo.onufriiev@applitools.com>
2022-06-09 22:18:11 +0300

    fix

--- .github/actions/run/index.js
+++ .github/actions/run/index.js
@@ -7,14 +7,14 @@ const ref = core.getInput('ref')
 console.log(workflow, ref)
 
 async function main() {
-  execSync('gh', ['workflow', 'run', workflow])
-  const {stdout} = execSync('gh', ['run', 'list', '--json', 'databaseId', '--workflow', workflow, '--limit', '1'])
+  execSync(`gh workflow run ${workflow}`)
+  const {stdout} = execSync(`gh run list --json databaseId --workflow ${workflow} --limit 1`, {encoding: 'utf8'})
 
   console.log(stdout)
 
   const [{databaseId}] = JSON.parse(stdout)
 
-  execSync('gh', ['run', 'watch', databaseId], {stdio: 'inherit'})
+  console.log(execSync(`gh run watch ${databaseId}`, {stdio: 'inherit'}))
 
 }
 
