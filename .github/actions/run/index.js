1533bedf3ff29c87f840b30e5996c75595db42f7
kyrylo <kyrylo.onufriiev@applitools.com>
2022-06-09 19:47:15 +0300

    fix

--- .github/actions/run/index.js
+++ .github/actions/run/index.js
@@ -4,9 +4,11 @@ import {spawn} from 'child_process'
 const workflow = core.getInput('workflow', {required: true})
 const ref = core.getInput('ref')
 
-const run = spawn(`gh workflow run ${workflow} --ref ${ref ?? '$(git rev-parse --abbrev-ref HEAD)'}`, {
+console.log(workflow, ref)
+
+const run = spawn(`gh workflow run ${workflow} --ref $(git rev-parse --abbrev-ref HEAD)`, {
   shell: true,
   stdio: 'inherit'
-})
+}, console.log)
 
 run.on('error', console.log)
