e00cb60ced2e4614006bdc5bdc710fab66084ba3
kyrylo <kyrylo.onufriiev@applitools.com>
2022-06-09 22:39:02 +0300

    g

--- .github/actions/run/index.js
+++ .github/actions/run/index.js
@@ -1,5 +1,6 @@
 import * as core from '@actions/core'
 import {execSync} from 'child_process'
+import {setTimeout} from 'timers/promises'
 
 const workflow = core.getInput('workflow', {required: true})
 const ref = core.getInput('ref')
@@ -8,6 +9,9 @@ console.log(workflow, ref)
 
 async function main() {
   execSync(`gh workflow run ${workflow}`)
+  
+  await setTimeout(5000)
+
   const g = execSync(`gh run list --json databaseId --workflow ${workflow} --limit 1`, {encoding: 'utf8'})
 
   console.log(g)
