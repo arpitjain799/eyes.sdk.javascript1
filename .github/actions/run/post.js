352a82d274828f4584f3ca7e25df0af44385cd7c
kyrylo <kyrylo.onufriiev@applitools.com>
2022-06-13 15:11:42 +0300

    fix

--- .github/actions/run/post.js
+++ .github/actions/run/post.js
@@ -1,6 +1,15 @@
+import * as github from '@actions/github'
+
 post()
 
 async function post() {
+  const response = await octokit.rest.actions.getWorkflowRunAttempt({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo,
+    run_id: github.context.runId,
+    attempt_number: github.context.runNumber,
+  })
   console.log('POST!')
   console.log(process.env)
+  console.log(response.data)
 }
