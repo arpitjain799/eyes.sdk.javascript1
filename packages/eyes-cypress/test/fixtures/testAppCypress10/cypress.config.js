8fd68c97e8054caedcf0ff10b2917663f9eb6c61
Sophie Tagar <sophietagar@Sophies-MacBook-Pro-2.local>
2022-06-30 10:12:36 -0700

    fix cypress.config.js file for tests

--- packages/eyes-cypress/test/fixtures/testAppCypress10/cypress.config.js
+++ packages/eyes-cypress/test/fixtures/testAppCypress10/cypress.config.js
@@ -6,10 +6,10 @@ module.exports = defineConfig({
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index-run.js')(on, config);
+    },
     specPattern: './cypress/integration-run',
     supportFile: './cypress/support/e2e.js',
   },
 });
-
-require('../../../')(module);
