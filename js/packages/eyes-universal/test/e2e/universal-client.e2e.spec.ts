bfb42f4118322f38ee519a7ecf7d54121620b4fd
Amit Zur <amit.zur@applitools.com>
2022-07-07 06:54:00 +0300

    e2e tests for universal server

---
+++ js/packages/eyes-universal/test/e2e/universal-client.e2e.spec.ts
@@ -0,0 +1,87 @@
+import {UniversalClient} from './utils/universal-client'
+import * as spec from '@applitools/spec-driver-selenium'
+import * as assert from 'assert'
+
+describe('Universal client', () => {
+  before(async () => {
+    await UniversalClient.killServer()
+  })
+
+  describe('Web', () => {
+    let driver, destroyDriver, client
+
+    beforeEach(async () => {
+      ;[driver, destroyDriver] = await spec.build({browser: 'chrome'})
+      await driver.get('https://applitools.github.io/demo/TestPages/FramesTestPage/')
+
+      client = new UniversalClient()
+    })
+
+    afterEach(async () => {
+      await destroyDriver()
+      await client.closeServer() // TODO remove
+    })
+
+    let timeoutId
+    beforeEach(() => {
+      timeoutId = setTimeout(() => console.log('ugly hack'), 1000000)
+    })
+    afterEach(() => {
+      clearTimeout(timeoutId)
+    })
+
+    it('works', async () => {
+      const config = {
+        appName: 'universal e2e tests',
+        testName: 'universal e2e test',
+        saveNewTests: false,
+        // browsersInfo: [{androidDeviceInfo: {deviceName: 'Pixel 4 XL', androidVersion: 'latest'}}],
+      }
+      // const manager = await client.makeManager({type: 'vg', concurrency: 5})
+      const manager = await client.makeManager({})
+      const eyes = await manager.openEyes({driver, config})
+      await eyes.check({
+        settings: {ignoreRegions: [{region: '#overflowing-div', padding: {left: 10, top: 20, right: 30, bottom: 40}}]},
+      })
+      const results = await eyes.close({throwErr: false})
+      assert.strictEqual(results[0].status, 'Passed')
+      const allResults = await manager.closeManager({throwErr: false})
+      assert.strictEqual(allResults.results[0].testResults.status, 'Passed')
+    })
+  })
+
+  describe.skip('Android', () => {
+    let driver, destroyDriver, client
+
+    beforeEach(async () => {
+      client = new UniversalClient({driverType: 'sauce'})
+      ;[driver, destroyDriver] = await spec.build({
+        device: 'Pixel 3 XL',
+        app: 'https://applitools.jfrog.io/artifactory/Examples/runnerup_multiple_checks.apk',
+      })
+    })
+
+    afterEach(async () => {
+      await destroyDriver()
+    })
+    it('works', async () => {
+      const timeoutId = setTimeout(() => console.log('ugly hack'), 1000000)
+
+      const config = {
+        appName: 'universal e2e tests',
+        testName: 'universal e2e test',
+        saveNewTests: false,
+        browsersInfo: [{androidDeviceInfo: {deviceName: 'Pixel 4 XL', androidVersion: 'latest'}}],
+      }
+      const manager = await client.makeManager({type: 'vg', concurrency: 5})
+      const eyes = await manager.openEyes({driver, config})
+      await eyes.check({settings: {}})
+      const results = await eyes.close({throwErr: false})
+      assert.strictEqual(results[0].status, 'Passed')
+
+      const allResults = await manager.closeManager({throwErr: false})
+      assert.strictEqual(allResults.results[0].testResults.status, 'Passed')
+      clearTimeout(timeoutId)
+    })
+  })
+})
---
+++ js/packages/eyes-universal/test/e2e/utils/transform-driver.ts
@@ -0,0 +1,43 @@
+import type * as types from '@applitools/types'
+import type {Driver, Element, Selector} from '@applitools/spec-driver-selenium'
+import * as spec from '@applitools/spec-driver-selenium'
+import * as utils from '@applitools/utils'
+
+export {Driver, Element, Selector}
+export type TransformedDriver = {sessionId: string; serverUrl: string; capabilities: Record<string, any>}
+export type TransformedElement = {elementId: string}
+export type TransformedSelector = types.Selector<never>
+
+export async function transform(data: any, driverUrl?: string): Promise<any> {
+  if (spec.isDriver(data)) {
+    return transformDriver(data, driverUrl)
+  } else if (spec.isElement(data)) {
+    return transformElement(data)
+  } else if (utils.types.isArray(data)) {
+    return Promise.all((data as Array<any>).map(x => transform(x)))
+  } else if (utils.types.isObject(data)) {
+    return Object.entries(data).reduce(async (data, [key, value]) => {
+      const transformed = await transform(value)
+      return data.then(data => Object.assign(data, {[key]: transformed}))
+    }, Promise.resolve({}))
+  } else {
+    return data
+  }
+}
+
+async function transformDriver(driver: Driver, driverUrl: string): Promise<TransformedDriver> {
+  const session = await driver.getSession()
+  const capabilities = await driver.getCapabilities()
+  return {
+    serverUrl: driverUrl,
+    sessionId: session.getId(),
+    capabilities: Array.from(capabilities.keys()).reduce((caps, key) => {
+      caps[key as string] = capabilities.get(key)
+      return caps
+    }, {} as Record<string, any>),
+  }
+}
+
+async function transformElement(element: Element): Promise<TransformedElement> {
+  return {elementId: await element.getId()}
+}
