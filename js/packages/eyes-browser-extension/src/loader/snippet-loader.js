2bd8254ccc35e46921d6a9b38657fd269f6cc7f9
ran.itzhaki <ran.itzhaki@applitools.com>
2022-10-25 19:56:14 +0300

    Make it more readable

--- js/packages/eyes-browser-extension/src/loader/snippet-loader.js
+++ js/packages/eyes-browser-extension/src/loader/snippet-loader.js
@@ -1,6 +1,18 @@
-module.exports = function (source) {
-  return source.replace(
-    /("use strict";)/g,
-    "$1arg=typeof ref !== 'undefiend'&&arg&&arg.map&&arg.map(s=>refer.isRef(s) ? refer.deref(s) : s);",
+// in order to support the refer in the snippet
+// this loader warp the snippet functions with
+// `refer.ref` for the arguments and `refer.deref`
+// for the return values
+module.exports = function () {
+  return Object.entries(require(this.resourcePath)).reduce(
+    (str, [name, func]) =>
+      str +
+      `exports.${name}=function(arg){
+        try {
+          return refer.ref((${func.toString()})(refer.deref(arg)))
+        } catch (error) {
+          return {error: error instanceof Error ? {message: error.message, stack: error.stack} : error}
+        }
+      }\n`,
+    '',
   )
 }
