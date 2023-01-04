de1114fa59e197d907a2abcbbf57ca0f3da8a0d5
Dave Haeffner <dhaeffner@gmail.com>
2023-01-04 10:00:49 -0500

    js core-base: when preparing the self healing report payload to be sent to eyes, use just the string value of the selector (instead of the object containing the string value and the strategy used)

--- js/packages/core-base/test/unit/to-self-healing-report.spec.ts
+++ js/packages/core-base/test/unit/to-self-healing-report.spec.ts
@@ -4,13 +4,20 @@ import {toSelfHealingReport} from '../../src/utils/to-self-healing-report'
 describe('transform', () => {
   it('driver-session-metadata to self-healing-report', async () => {
     const input = [
-      {successfulSelector: 'a', originalSelector: 'b'},
-      {successfulSelector: 'b', originalSelector: 'c'},
-      {successfulSelector: 'c', originalSelector: 'd'},
+      {
+        successfulSelector: { using: 'xpath', value: '//blah' },
+        message: 'found with saved selector',
+        originalSelector: { using: 'css selector', value: '#blah' }
+      },
+      {
+        successfulSelector: { using: 'xpath', value: '//*[@href="/app.html" ]' },
+        message: 'found with saved selector',
+        originalSelector: { using: 'css selector', value: '#log-in' }
+      }
     ]
     toSelfHealingReport(input).operations.forEach((result, index) => {
-      assert.deepStrictEqual(result.old, input[index].originalSelector)
-      assert.deepStrictEqual(result.new, input[index].successfulSelector)
+      assert.deepStrictEqual(result.old, input[index].originalSelector.value)
+      assert.deepStrictEqual(result.new, input[index].successfulSelector.value)
       assert(Date.parse(result.timestamp))
     })
   })
