d4bd104b158adf9ff9e88b344df28b4040bb4318
artem <artem.borodavka@applitools.com>
2023-03-07 18:29:52 +0200

    update post version

--- python/ci_scripts/bump_test.sh
+++ python/ci_scripts/bump_test.sh
@@ -8,9 +8,7 @@ echo "$basedir"
 
 
 cd "$basedir"/eyes_selenium || exit
-bumpversion build --list --config-file "$(pwd)/.bumptest.cfg"
+bumpversion post --list --config-file "$(pwd)/.bumptest.cfg"
 echo "eyes_selenium is done"
 cd "$basedir"/core_universal || exit
-current_version="$(grep current_version setup.cfg | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+\(\.[a-zA-Z]\+\)\?')"
-new_version="$current_version".test
-bumpversion post --new-version "$new_version" --list
+bumpversion post --list
