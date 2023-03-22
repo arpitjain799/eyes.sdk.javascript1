adbeed2ada6b69c94ad49d344406b9d7aad4c158
ran.itzhaki <ran.itzhaki@applitools.com>
2023-03-22 16:23:20 +0200

    fix(js): just for testing

--- js/packages/core/src/index.ts
+++ js/packages/core/src/index.ts
@@ -5,6 +5,5 @@ export * from './universal/core-server-process'
 
 export {type SpecType, type SpecDriver, type Selector} from '@applitools/driver'
 export * from '@applitools/driver/dist/debug'
-
 export * as formatters from './utils/format-results'
 export {takeDomSnapshots} from './ufg/utils/take-dom-snapshots'
