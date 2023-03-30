7ebefefee60c2792998a63e5c8adbaf7f630be53
ran.itzhaki <ran.itzhaki@applitools.com>
2023-03-30 15:48:45 +0200

    test: upgrade e2e to cypress@latest

--- js/packages/eyes-cypress/test/e2e/cypress-run-simple.test.js
+++ js/packages/eyes-cypress/test/e2e/cypress-run-simple.test.js
@@ -1,36 +1,30 @@
 'use strict'
 const {describe, it, before, after} = require('mocha')
 const path = require('path')
-const pexec = require('../util/pexec')
-const fs = require('fs')
 
 const sourceTestAppPath = path.resolve(__dirname, '../fixtures/testApp')
 const targetTestAppPath = path.resolve(__dirname, '../fixtures/testAppCopies/testApp-simple')
 
-describe('simple', () => {
+const cypressUtils = require('../util').cypressUtils({targetTestAppPath, sourceTestAppPath})
+const {runCypress, updateConfigFile} = cypressUtils
+
+describe('match level', () => {
   before(async () => {
-    if (fs.existsSync(targetTestAppPath)) {
-      fs.rmdirSync(targetTestAppPath, {recursive: true})
-    }
-    await pexec(`cp -r ${sourceTestAppPath}/. ${targetTestAppPath}`)
-    process.chdir(targetTestAppPath)
-    await pexec(`npm install`, {
-      maxBuffer: 1000000,
-    })
+    await cypressUtils.before()
   })
 
   after(async () => {
-    fs.rmdirSync(targetTestAppPath, {recursive: true})
+    await cypressUtils.after()
+  })
+
+  beforeEach(async () => {
+    await cypressUtils.beforeEach()
   })
 
   it('works for simple.js', async () => {
     try {
-      await pexec(
-        './node_modules/.bin/cypress run --headless --config testFiles=simple.js,integrationFolder=cypress/integration-run,pluginsFile=cypress/plugins/index-run.js,supportFile=cypress/support/index-run.js',
-        {
-          maxBuffer: 10000000,
-        },
-      )
+      await updateConfigFile('index-run.js', 'simple.js')
+      await runCypress()
     } catch (ex) {
       console.error('Error during test!', ex.stdout)
       throw ex
--- js/packages/eyes-cypress/test/fixtures/testApp/cypress/integration-run/simple.js
+++ js/packages/eyes-cypress/test/fixtures/testApp/cypress/integration-run/simple.js
@@ -1,30 +1,30 @@
-/* global cy */
+/* global cy, Cypress */
 describe('eyes-cypress', () => {
   // This also tests the setting of `testName` inside `it`
 
   it('simple', () => {
-    cy.setCookie('auth', 'secret');
-    const url = `http://localhost:${Cypress.config('testPort')}/test.html`;
-    cy.visit(url);
+    cy.setCookie('auth', 'secret')
+    const url = `http://localhost:${Cypress.config('testPort')}/test.html`
+    cy.visit(url)
     cy.eyesOpen({
       appName: 'some app',
       browser: {width: 1024, height: 768},
       // showLogs: true,
-    });
-    cy.eyesCheckWindow('full page');
-    cy.eyesClose();
-  });
+    })
+    cy.eyesCheckWindow('full page')
+    cy.eyesClose()
+  })
 
   it('simple with matchLevel', () => {
-    cy.setCookie('auth', 'secret');
-    const url = `http://localhost:${Cypress.config('testPort')}/match-level.html`;
-    cy.visit(url);
+    cy.setCookie('auth', 'secret')
+    const url = `http://localhost:${Cypress.config('testPort')}/match-level.html`
+    cy.visit(url)
     cy.eyesOpen({
       appName: 'some app',
       browser: {width: 1024, height: 768},
       matchLevel: 'Layout',
-    });
-    cy.eyesCheckWindow('full page');
-    cy.eyesClose();
-  });
-});
+    })
+    cy.eyesCheckWindow('full page')
+    cy.eyesClose()
+  })
+})
