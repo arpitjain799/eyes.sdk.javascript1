7044e07d29479ef231320ab440660dc7b329caae
Dave Haeffner <dhaeffner@gmail.com>
2023-04-14 21:44:47 -0400

    js core: improve header check in ua-middleware and include logging of the user agent that was received

--- js/packages/core/test/fixtures/middlewares/ua-middleware.js
+++ js/packages/core/test/fixtures/middlewares/ua-middleware.js
@@ -1,11 +1,9 @@
 exports.create = function ({userAgent}) {
   return (req, res, next) => {
     if (req.url.endsWith('.jpg')) {
-      if (req.headers['user-agent'] !== userAgent) {
-        res.status(404).send('Not found')
-      } else {
-        next()
-      }
+      console.log('user-agent', req.headers['user-agent'])
+      if (new RegExp(userAgent).test(req.headers['user-agent'])) next()
+      else res.status(404).send('Not found')
     } else {
       next()
     }
